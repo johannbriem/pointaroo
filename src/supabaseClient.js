08d9fedd29d6fde2c8bb43433d66d2625e550f29
johannbriem <johannbriem@gmail.com>
2025-06-22 18:19:36 +0000

    Moving supabase to new directory

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dvtgizlrzukgboxbkioe.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2dGdpemxyenVrZ2JveGJraW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NTU5MTcsImV4cCI6MjA2NjAzMTkxN30.XICCHXnXcitXc3wEuNLiY_LPoT3Em8XpWbrFFHvWbjM';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
